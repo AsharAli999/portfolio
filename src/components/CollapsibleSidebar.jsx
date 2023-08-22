b0a706dd8e48159fb93f4375058c9d78d89430c6
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-08-22 22:56:10 +0500

    adjust some ui issues

--- src/components/CollapsibleSidebar.jsx
+++ src/components/CollapsibleSidebar.jsx
@@ -47,7 +47,7 @@ const CollapsibleSidebar = () => {
         <div className="px-4 py-2 border-b border-gray-700">
           <div className="flex items-center space-x-2">
             <FaCode className="text-blue-400" />
-            <span className="text-lg font-semibold">VS Code</span>
+            <span className="text-lg font-semibold">My Code</span>
           </div>
         </div>
         <div className="flex-1 overflow-y-auto">
